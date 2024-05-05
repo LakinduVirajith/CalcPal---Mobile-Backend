731c3642dfccf80cf3eaa1fd5dbfc7fa96181500
Sankalpa2000 <Sankalpa.pasindu7@gmail.com>
2024-05-05 23:25:36 +0530

    Visual-spatial dignosis backend  part completed

---
+++ visual-diagnosis-service/src/main/java/com/calcpal/visualdiagnosisservice/DTO/DiagnosisLabelDTO.java
@@ -0,0 +1,20 @@
+package com.calcpal.visualdiagnosisservice.DTO;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DiagnosisLabelDTO {
+
+    @NotNull
+    private String userEmail;
+
+    @NotNull
+    private Boolean label;
+}
