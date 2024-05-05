1aae991c4ce489e1fa5d277e12c0cbf9eac907e2
Sankalpa2000 <Sankalpa.pasindu7@gmail.com>
2024-05-05 17:57:41 +0530

    config file added

---
+++ visual-diagnosis-service/src/test/java/com/calcpal/visualdiagnosisservice/VisualDiagnosisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcpal.visualdiagnosisservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualDiagnosisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
