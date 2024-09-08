f59fb1d004a6325357295ed73115bd61a4f3e906
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-09-08 13:50:41 +0530

    api controller error resolve practognostic

--- practognostic-diagnosis-service/src/main/java/com/calcpal/practognosticdiagnosisservice/controller/PractognosticActivityController.java
+++ practognostic-diagnosis-service/src/main/java/com/calcpal/practognosticdiagnosisservice/controller/PractognosticActivityController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/lexical/activity")
-@Tag(name = "Lexical Activity Controllers")
+@RequestMapping("/api/v1/practognosticA/activity")
+@Tag(name = "Practognostic Activity Controllers")
 @RequiredArgsConstructor
 public class PractognosticActivityController {
 
