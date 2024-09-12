bb8e18141c22566efd1db2888a1d401eadcf1f11
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-09-12 19:58:54 +0530

    Revert "remove all files for branch resolvement"
    This reverts commit 295689bd647b008f80e13ac7660ec9d1919d68df.

---
+++ setup_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# REPLACE PLACEHOLDERS IN USER SERVICE YAML
+sed -i "s|\${JWT_SECRET}|$JWT_SECRET|g" user-service/src/main/resources/application.yaml
+sed -i "s|\${MAIL_ADDRESS}|$MAIL_ADDRESS|g" user-service/src/main/resources/application.yaml
+sed -i "s|\${MAIL_APP_PASSWORD}|$MAIL_APP_PASSWORD|g" user-service/src/main/resources/application.yaml
+sed -i "s|\${MONGODB_URI}|$MONGODB_URI|g" config-server/src/main/resources/config/user-service.yaml
+
+# REPLACE PLACEHOLDERS IN VERBAL DIAGNOSIS YAML
+sed -i "s|\${MONGODB_URI}|$MONGODB_URI|g" config-server/src/main/resources/config/verbal-diagnosis-service.yaml
+
+# REPLACE PLACEHOLDERS IN LEXICAL DIAGNOSIS YAML
+sed -i "s|\${MONGODB_URI}|$MONGODB_URI|g" config-server/src/main/resources/config/lexical-diagnosis-service.yaml
+
+# REPLACE PLACEHOLDERS IN OPERATIONAL DIAGNOSIS YAML
+sed -i "s|\${MONGODB_URI}|$MONGODB_URI|g" config-server/src/main/resources/config/operational-diagnosis-service.yaml
